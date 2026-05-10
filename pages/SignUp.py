06b09f416d24853fb9a4c341693099a4d8c77d14
Anuj kumar yadav <anujku7654906304@gmail.com>
2026-05-10 18:57:32 +0530

    Update MongoDB connection details in SignUp.py

--- pages/SignUp.py
+++ pages/SignUp.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pymongo
-myclient=pymongo.MongoClient("mongodb+srv://anujkumarr45:75lTY2QWHmIYe9AO@cluster0.ukwltkn.mongodb.net/?appName=Cluster0")
+myclient=pymongo.MongoClient("mongodb+srv://lalu6365d_db_user:3REHd1eJzvesy46L@cluster0.kdswfix.mongodb.net/?appName=Cluster0")
 mydb=myclient["Bitcoin"]
 my=mydb["system"]
 st.title(":blue[Sign Up]")
